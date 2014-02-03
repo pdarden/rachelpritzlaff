1674e4c02a3a9e4444a5b433e71c9f6fbeefc3fc
Parinda Darden <parinda.darden@gmail.com>
2014-02-03 16:17:04 -0500

    Made pages and custom routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def bio
+  end
+
+  def performance
+  end
+
+  def up_coming
+  end
+
+  def subscribe
+  end
+
+  def contact
+  end
+end
